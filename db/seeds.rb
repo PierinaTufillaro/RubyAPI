7aa00afa5265dacb501bd405171a9fa80050841e
Pierina Tufillaro <pierinatufillaro@hotmail.com>
2020-02-04 12:47:56 -0300

    modifico seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
- User.create!(email:"admin98@admin.com",password:"12345678")
- Client.create!(cuit_cuil:"123456/6", name:"Pierina", email:"pierinaTufillaro98@email.com", iva_cond: 1)
- Product.create!(code:"PPP444", short_description:"Bebida", long_description: "Gaseosa Cola", 
+ User.find_or_create_by!(email:"admin98@admin.com",password:"12345678")
+ Client.find_or_create_by!(cuit_cuil:"123456/6", name:"Pierina", email:"pierinaTufillaro98@email.com", iva_cond: 1)
+ p = Product.find_or_create_by!(code:"PPP444", short_description:"Bebida", long_description: "Gaseosa Cola", 
  	unit_amount: 10, stock:2)
- Item.create!(status:0, product_id:1)
- Item.create!(status:0, product_id:1)
+ Item.create!(status:0, product: p)
+ Item.create!(status:0, product: p)
 
